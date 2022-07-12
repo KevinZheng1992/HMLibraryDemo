9e86157ea62dca8eb365a05486da1dcc16278bd9
Yan <1030397448@qq.com>
2022-07-13 01:24:09 +0800

    initial project setup

---
+++ HMLibraryDemo/HMLibraryDemo/HMLibraryDemo.h
@@ -0,0 +1,18 @@
+//
+//  HMLibraryDemo.h
+//  HMLibraryDemo
+//
+//  Created by kevinzheng on 2022/7/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HMLibraryDemo.
+FOUNDATION_EXPORT double HMLibraryDemoVersionNumber;
+
+//! Project version string for HMLibraryDemo.
+FOUNDATION_EXPORT const unsigned char HMLibraryDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMLibraryDemo/PublicHeader.h>
+
+
